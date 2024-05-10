06b56945d6113bf377185cf3e0312c57b26b4827
Matias Novillo <novillo.matias1@gmail.com>
2024-05-10 09:03:41 -0300

    Se reemplazo la BD local por una en la nube

--- Areas/JuanAppContext.cs
+++ Areas/JuanAppContext.cs
@@ -28,7 +28,7 @@ namespace JuanApp.Areas.BasicCore
             try
             {
                 string ConnectionString = "";
-
+#if DEBUG
                 ConnectionString = "data source =.; " +
                    "initial catalog = JuanApp; " +
                    "Integrated Security = SSPI;" +
@@ -37,7 +37,17 @@ namespace JuanApp.Areas.BasicCore
                    "Persist Security Info=True;" +
                    "App=EntityFramework;" +
                    "TrustServerCertificate=True;";
+#else
+                string IP = "192.168.28.14";
+                string Server = "sql4.baehost.com";
 
+                ConnectionString = "Password=Zc2s4~7n0;" +
+                    "Persist Security Info=True;" +
+                    "User ID=fiyista1_JuanAppAdmin;" +
+                    "Initial Catalog=fiyista1_JuanApp;" +
+                    "Data Source=sql4.baehost.com;" +
+                    "TrustServerCertificate=True";
+#endif
                 optionsBuilder
                     .UseSqlServer(ConnectionString);
             }
